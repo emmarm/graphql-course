4fe818826804a6180e6ace8296582b97cb8a37f0
Emma M <mdopure@gmail.com>
2018-10-07 00:04:32 -0700

    mutations use object spread for args

--- src/index.js
+++ src/index.js
@@ -163,9 +163,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age
+        ...args
       };
 
       users.push(user);
@@ -180,10 +178,7 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author
+        ...args
       };
 
       posts.push(post);
