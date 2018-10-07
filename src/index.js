bdf12f6941db3f4e9aa65548a99f04c446fd9a84
Emma M <mdopure@gmail.com>
2018-10-07 00:24:27 -0700

    add deletePost and deleteComment mutations

--- src/index.js
+++ src/index.js
@@ -84,7 +84,9 @@ const typeDefs = `
     createUser(data: CreateUserInput): User!
     deleteUser(id: ID!): User!
     createPost(data: CreatePostInput): Post!
+    deletePost(id: ID!): Post!
     createComment(data: CreateCommentInput!): Comment!
+    deleteComment(id: ID!): Comment!
   }
 
   input CreateUserInput {
@@ -227,6 +229,19 @@ const resolvers = {
       posts.push(post);
       return post;
     },
+    deletePost(parent, args, ctx, info) {
+      const postIndex = posts.findIndex(post => post.id === args.id);
+
+      if (postIndex === -1) {
+        throw new Error("Post not found");
+      }
+
+      const deletedPosts = posts.splice(postIndex, 1);
+
+      comments = comments.filter(comment => comment.post !== args.id);
+
+      return deletedPosts[0];
+    },
     createComment(parent, args, ctx, info) {
       const authorFound = users.some(user => user.id === args.data.author);
       const postFound = posts.some(
@@ -247,6 +262,18 @@ const resolvers = {
       comments.push(comment);
 
       return comment;
+    },
+    deleteComment(parent, args, ctx, info) {
+      const commentIndex = comments.findIndex(
+        comment => comment.id === args.id
+      );
+
+      if (commentIndex === -1) {
+        throw new Error("Comment not found");
+      }
+
+      const deletedComments = comments.splice(commentIndex, 1);
+      return deletedComments[0];
     }
   },
   Post: {
