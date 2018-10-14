5bcd8f85b4e4ce01faa6706ef1444a82ad3ad1cc
Emma M <mdopure@gmail.com>
2018-10-14 16:25:22 -0700

    add User posts field resolver

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,16 +1,31 @@
 import getUserId from "../utils/getUserId";
 
+const userIdFragment = "fragment userId on User { id }";
+
 const User = {
   email: {
-    fragment: "fragment userId on User { id }",
-    resolve(parent, args, { request }, info) {
+    fragment: userIdFragment,
+    resolve({ id, email }, args, { request }, info) {
       const userId = getUserId(request);
 
-      if (userId && userId === parent.id) {
-        return parent.email;
+      if (userId && userId === id) {
+        return email;
       }
 
       return null;
+    }
+  },
+  posts: {
+    fragment: userIdFragment,
+    resolve(parent, args, { prisma }, info) {
+      return prisma.query.posts({
+        where: {
+          author: {
+            id
+          },
+          published: true
+        }
+      });
     }
   }
 };
