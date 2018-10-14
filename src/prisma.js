37dd1f67cd45a7676b2917bf82edbe9c356e3823
Emma M <mdopure@gmail.com>
2018-10-14 16:17:50 -0700

    set up support for fragmentReplacements

--- src/prisma.js
+++ src/prisma.js
@@ -1,9 +1,12 @@
 import { Prisma } from "prisma-binding";
 
+import { fragmentReplacements } from "./resolvers";
+
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   endpoint: "http://localhost:4466",
-  secret: "whoasosecret"
+  secret: "whoasosecret",
+  fragmentReplacements
 });
 
 export default prisma;
