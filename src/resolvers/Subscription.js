c346634e495c6d8f8327808aa951c02da2f22fb8
Emma M <mdopure@gmail.com>
2018-10-14 16:51:53 -0700

    add authenticated myPosts subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,13 +1,22 @@
+import getUserId from "../utils/getUserId";
+
 const Subscription = {
   post: {
     subscribe(parent, args, { prisma }, info) {
       return prisma.subscription.post(
         {
-          where: {
-            node: {
-              published: true
-            }
-          }
+          where: { node: { published: true } }
+        },
+        info
+      );
+    }
+  },
+  myPost: {
+    subscribe(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request);
+      return prisma.subscription.post(
+        {
+          where: { node: { author: { id: userId } } }
         },
         info
       );
